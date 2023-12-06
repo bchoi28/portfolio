4b18e11241d4499336d0cc4363d6f114c8a0dd6b
Brandon Choi <bchoi28@gmail.com>
2023-12-05 23:55:08 -0500

    sendMessage async function template

--- src/components/Branbot.jsx
+++ src/components/Branbot.jsx
@@ -7,11 +7,32 @@ const Chatbot = () => {
 
     const toggleChat = () => setIsOpen(!isOpen);
 
-    const sendMessage = (message) => {
-        // Here you would integrate with AWS Lambda and API Gateway
-        // For now, let's just add the message to our state
-        setMessages([...messages, { sender: 'user', text: message }]);
+    const sendMessage = async (userMessage) => {
+        // Add the user's message to the chat
+        setMessages(prevMessages => [...prevMessages, { sender: 'user', text: userMessage }]);
+
+        try {
+            // Send the message to your Lambda function via API Gateway
+            const response = await fetch('https://your-api-gateway-url.com/branbot', {
+                method: 'POST',
+                body: JSON.stringify({ message: userMessage }),
+                headers: { 'Content-Type': 'application/json' }
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = await response.json();
+
+            // Add the bot's response to the chat
+            setMessages(prevMessages => [...prevMessages, { sender: 'bot', text: data.reply }]);
+        } catch (error) {
+            console.error('Error:', error);
+            // Optionally, handle the error in the UI, for example, by showing an error message
+        }
     };
+
 
     return (
         <div className="chatbot-container">
